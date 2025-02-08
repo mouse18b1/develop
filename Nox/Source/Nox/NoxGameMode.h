092abe5c032f51fa9b0d90016b8201238f1c5a62
Hayashi Shoei <syooo7@yahoo.co.jp>
2025-02-08 11:08:38 +0900

    最初のコミット

---
+++ Nox/Source/Nox/NoxGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NoxGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANoxGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANoxGameMode();
+};
+
+
+
